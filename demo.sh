dad967cf391f3eae7806455c773fc7b1c5a8b2c6
Tim Gross <tim@0x74696d.com>
2016-04-04 16:20:57 -0400

    Clean docker exec out of demo script

--- demo.sh
+++ demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PREFIX=ccic
-export DOCKER_CLIENT_TIMEOUT=300
+export PREFIX=cb
+export COMPOSE_TIMEOUTDOCKER_CLIENT_TIMEOUT=300
 
 echo 'Starting Couchbase cluster'
 
@@ -11,44 +11,24 @@ docker-compose pull
 
 echo
 echo 'Starting containers'
-docker-compose --project-name=$PREFIX up -d --no-recreate --timeout=300
+docker-compose --project-name=${PREFIX} up -d --no-recreate
 
-echo
-echo -n 'Initilizing cluster.'
-
-sleep 1.3
-COUCHBASERESPONSIVE=0
-while [ $COUCHBASERESPONSIVE != 1 ]; do
-    echo -n '.'
-
-    RUNNING=$(docker inspect "${PREFIX}_couchbase_1" | json -a State.Running)
-    if [ "$RUNNING" == "true" ]
-    then
-        docker exec -it "${PREFIX}_couchbase_1" triton-bootstrap bootstrap benchmark
-        let COUCHBASERESPONSIVE=1
-    else
-        sleep 1.3
-    fi
-done
-echo
-
-CONSUL=$("triton ip ${PREFIX}_consul_1"):8500
+CONSUL="$(triton ip ${PREFIX}_consul_1):8500"
 echo
 echo 'Consul is now running'
 echo "Dashboard: $CONSUL"
 command -v open >/dev/null 2>&1 && `open http://$CONSUL/ui/`
 
-CBDASHBOARD=$("triton ip ${PREFIX}_couchbase_1"):8091
+CBDASHBOARD="$(triton ip ${PREFIX}_couchbase_1):8091"
 echo
 echo 'Couchbase cluster running and bootstrapped'
 echo "Dashboard: $CBDASHBOARD"
-echo 'The username and password are printed in earlier messages'
 command -v open >/dev/null 2>&1 && `open http://$CBDASHBOARD/index.html#sec=servers`
 
 echo
 echo 'Scaling Couchbase cluster to three nodes'
 echo "docker-compose --project-name=$PREFIX scale couchbase=3"
-docker-compose --project-name=$PREFIX scale couchbase=3
+docker-compose --project-name=${PREFIX} scale couchbase=3
 
 echo
 echo "Go ahead, try a lucky 7 node cluster:"
